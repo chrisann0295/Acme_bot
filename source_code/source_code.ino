9b02c1c596c1eb71b6b24989cf1dcf3c40c06bf5
Chrisanda Ann <chrisann@uvic.ca>
2019-01-13 11:58:18 +0530

    wth

--- source_code/source_code.ino
+++ source_code/source_code.ino
@@ -17,10 +17,13 @@ void setup() {
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
+  Serial.begin(9600);
 }
 void loop() {
   int xAxis = analogRead(A0); // Read Joysticks X-axis
   int yAxis = analogRead(A1); // Read Joysticks Y-axis
+
+  Serial.write(yAxis);
   // Y-axis used for forward and backward control
   if (yAxis < 470) {
     // Set Motor A backward
@@ -50,39 +53,39 @@ void loop() {
     motorSpeedB = 0;
   }
   // X-axis used for left and right control
-  if (xAxis < 470) {
-    // Convert the declining X-axis readings from 470 to 0 into increasing 0 to 255 value
-    int xMapped = map(xAxis, 470, 0, 0, 255);
-    // Move to left - decrease left motor speed, increase right motor speed
-    motorSpeedA = motorSpeedA - xMapped;
-    motorSpeedB = motorSpeedB + xMapped;
-    // Confine the range from 0 to 255
-    if (motorSpeedA < 0) {
-      motorSpeedA = 0;
-    }
-    if (motorSpeedB > 255) {
-      motorSpeedB = 255;
-    }
-  }
-  if (xAxis > 550) {
-    // Convert the increasing X-axis readings from 550 to 1023 into 0 to 255 value
-    int xMapped = map(xAxis, 550, 1023, 0, 255);
-    // Move right - decrease right motor speed, increase left motor speed
-    motorSpeedA = motorSpeedA + xMapped;
-    motorSpeedB = motorSpeedB - xMapped;
-    // Confine the range from 0 to 255
-    if (motorSpeedA > 255) {
-      motorSpeedA = 255;
-    }
-    if (motorSpeedB < 0) {
-      motorSpeedB = 0;
-    }
-  }
-  // Prevent buzzing at low speeds (Adjust according to your motors. My motors couldn't start moving if PWM value was below value of 70)
-  if (motorSpeedA < 70) {
+//  if (xAxis < 470) {
+//    // Convert the declining X-axis readings from 470 to 0 into increasing 0 to 255 value
+//    int xMapped = map(xAxis, 470, 0, 0, 255);
+//    // Move to left - decrease left motor speed, increase right motor speed
+//    motorSpeedA = motorSpeedA - xMapped;
+//    motorSpeedB = motorSpeedB + xMapped;
+//    // Confine the range from 0 to 255
+//    if (motorSpeedA < 0) {
+//      motorSpeedA = 0;
+//    }
+//    if (motorSpeedB > 255) {
+//      motorSpeedB = 255;
+//    }
+//  }
+//  if (xAxis > 550) {
+//    // Convert the increasing X-axis readings from 550 to 1023 into 0 to 255 value
+//    int xMapped = map(xAxis, 550, 1023, 0, 255);
+//    // Move right - decrease right motor speed, increase left motor speed
+//    motorSpeedA = motorSpeedA + xMapped;
+//    motorSpeedB = motorSpeedB - xMapped;
+//    // Confine the range from 0 to 255
+//    if (motorSpeedA > 255) {
+//      motorSpeedA = 255;
+//    }
+//    if (motorSpeedB < 0) {
+//      motorSpeedB = 0;
+//    }
+//  }
+//  // Prevent buzzing at low speeds (Adjust according to your motors. My motors couldn't start moving if PWM value was below value of 70)
+  if (motorSpeedA < 100) {
     motorSpeedA = 0;
   }
-  if (motorSpeedB < 70) {
+  if (motorSpeedB < 100) {
     motorSpeedB = 0;
   }
   analogWrite(enA, motorSpeedA); // Send PWM signal to motor A
